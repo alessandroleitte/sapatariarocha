0c83fd60ca401e33f38525858699a1c0eedc89a3
alessandroleitte <65920383+alessandroleitte@users.noreply.github.com>
2025-12-09 16:07:53 -0300

    Refactor scroll event to manage btnTopo and btnWpp
    Consolidate scroll event handling for btnTopo and btnWpp.

--- script/script.js
+++ script/script.js
@@ -1,28 +1,19 @@
 const botaoMenu = document.getElementById("botao-menu"); // Exemplo
 const menu = document.getElementById("menu-mobile-itens"); //
+const btnTopo = document.getElementById('btn-topo');
+const btnWpp = document.getElementById('btn-wpp');
+const scrollThreshold = 300;
 
 botaoMenu.addEventListener("click", function () {
   menu.classList.toggle("aberto");
 });
 
-const btnTopo = document.getElementById('btn-topo');
-const scrollThreshold = 300;
-
 window.addEventListener('scroll', function() {
     if (window.scrollY > scrollThreshold) {
         btnTopo.style.display = 'flex'; 
-    } else {
-        btnTopo.style.display = 'none';
-    }
-});
-
-const btnWpp = document.getElementById('btn-wpp');
-
-window.addEventListener('scroll', function() {
-    if (window.scrollY > scrollThreshold) {
         btnWpp.style.display = 'flex';
     } else {
+        btnTopo.style.display = 'none';
         btnWpp.style.display = 'none';
-    } 
+    }
 });
-
